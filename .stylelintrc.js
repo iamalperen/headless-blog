5d1b72195d291dc7c3055ee56ed595d3a7fbf24a
Alperen Talaslıoğlu <alperen.talaslioglu@transavia.com>
2024-10-16 13:09:17 +0300

    husky

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,14 +8,11 @@ module.exports = {
         "stylelint-order"
     ],
     rules: {
-        // Use the 'stylistic/' prefix for stylistic rules
         "stylistic/string-quotes": "single",
         "stylistic/indentation": 2,
         "stylistic/max-empty-lines": 1,
         "stylistic/no-eol-whitespace": true,
-        // Property order
         "order/properties-alphabetical-order": true,
-        // Other custom rules as needed
     },
     ignoreFiles: [
         "**/node_modules/**",
